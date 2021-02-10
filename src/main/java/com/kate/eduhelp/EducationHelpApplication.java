bcbeba1654b6d33ed0c1b5109332d73358bada3e
Katesmir04 <Katecmirnova0407@mail.ru>
2021-02-10 20:33:20 +0300

    init

---
+++ src/main/java/com/kate/eduhelp/EducationHelpApplication.java
@@ -0,0 +1,13 @@
+package com.kate.eduhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationHelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kate/eduhelp/EducationHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kate.eduhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
